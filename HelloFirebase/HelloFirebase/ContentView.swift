36db14fece86ccfe34bdc1cb82db1b31292ddbc0
JohelCZarco <101434425+joelzarco@users.noreply.github.com>
2023-07-24 18:26:35 -0600

    f2: tasks saved to cloudFirestore :+1:

--- HelloFirebase/HelloFirebase/ContentView.swift
+++ HelloFirebase/HelloFirebase/ContentView.swift
@@ -13,14 +13,32 @@ import FirebaseFirestoreSwift
 struct ContentView: View {
     
     let db = Firestore.firestore()
+    @State private var title : String = ""
     
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+            TextField("Enter task", text: $title)
+                .textFieldStyle(.roundedBorder)
+            Button("Save"){  // this whole process will be moved to view model
+                let task = Task(title: title)
+                saveTask(task: task)
+            } // bttn
+        } // vs
         .padding()
     }
+    
+    private func saveTask(task : Task){
+        do{ // creates a new collection named tasks
+            try db.collection("tasks").addDocument(from: task){ err in
+                if let err = err{
+                    print(err.localizedDescription)
+                }else{
+                    title = ""
+                    print("successfully saved")
+                }
+            }
+        }catch{
+            print(error.localizedDescription)
+        }
+    } // saveTask
 }
---
+++ HelloFirebase/HelloFirebase/Models/Task.swift
@@ -0,0 +1,12 @@
+//
+//  Task.swift
+//  HelloFirebase
+//
+//  Created by sergio joel camacho zarco on 24/07/23.
+//
+
+import Foundation
+
+struct Task : Codable{
+    let title : String
+}
